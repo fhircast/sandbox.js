ba1e2b17a9863e85c01045d74432f37b9fa8ab64
Martin Bellehumeur <martin@Martins-Air.fritz.box>
2018-10-04 11:21:34 +0200

    start

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # FHIRCastJS
 JavaScript/Node Playground for FHIRCast
+
+This is a minimalistic and partial implementation of the FHIRcast proposal that I am putting together for short "hands on training" sessions.
+
+
---
+++ endpoints.js
@@ -0,0 +1,28 @@
+//      Endpoints Method  Payload(Content type)               Function
+//
+//  These are for the FHIRcast messages:
+//    Server (hub)
+//      /api/hub  POST    form query string     Receive subscription request from the cleints
+//      /notify   POST    JSON                  Receive events from the clients
+//
+//      /websocket
+//
+//
+//    Client
+//      /client   POST    JSON                  Receive events and subscribtion cancelations from the hub
+//      /client   GET     query string          Process callback check from the hub 
+//
+//  This endpoint is to provide the client UI (FHIRcast.html file): 
+//
+//    /ui         GET     HTML/JavaScript       Provides the web page to subscript and post events
+
+const listeningPort=6001;
+const request=require('request');
+const express=require('express'), 
+    app=express();
+var morgan = require('morgan');
+var bodyParser=require('body-parser');
+var path = require('path');
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended:true}));
