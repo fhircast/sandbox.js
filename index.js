72a313d81bd7d44488adf7abcdf177752b11bf71
Martin Bellehumeur <martin_bellehumeur@yahoo.com>
2018-10-11 08:14:24 +0200

    ip

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended:true}));
 var expressWs = require('express-ws')(app);
 
+var os = require( 'os' );
+const ifaces = os.networkInterfaces( );
+
+
 
 //  Global
 const port= process.env.PORT || 3000;
@@ -147,6 +151,27 @@ app.ws('/log', function(ws, req) {
 app.listen(port,function(){
   //console_log(help);
   console_log('🔥FHIRcast hub and client listening on port ' + port+'. ');
-    
+  Object.keys(ifaces).forEach(function (ifname) {
+    var alias = 0;
+  
+    ifaces[ifname].forEach(function (iface) {
+      if ('IPv4' !== iface.family || iface.internal !== false) {
+        // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+        return;
+      }
+  
+      if (alias >= 1) {
+        // this single interface has multiple ipv4 addresses
+        console.log(ifname + ':' + alias, iface.address);
+      } else {
+        // this interface has only one ipv4 adress
+        console.log(ifname, iface.address);
+      }
+      ++alias;
+    });
+  });
+  
+//  console_log( 'IP address: '+networkInterfaces['en0'][1].address ); 
+  //console_log( networkInterfaces ); 
 });
 
