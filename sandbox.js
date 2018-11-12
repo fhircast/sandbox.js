e93072586f64b2b57dac64c419f7c0ba3fa6c91a
Martin Bellehumeur <martin_bellehumeur@yahoo.com>
2018-11-12 16:10:25 +0100

    DocUpdate

--- sandbox.js
+++ sandbox.js
@@ -11,7 +11,7 @@ const expressWs = require('express-ws')(app);
 app.use(morgan('dev'));
 app.use(express.json());  
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(favicon(path.join(__dirname + '/fhir.ico')))
+app.use(favicon(path.join(__dirname + '/fhircast.png')))
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
